22527f59efadee8706e85bf905ad5925938c1d51
Chu Quang Hải <76799685+quanghaik2@users.noreply.github.com>
2023-02-14 20:19:01 +0700

    hai

--- app/src/main/java/com/example/datphongkhachsan/LoginActivity.java
+++ app/src/main/java/com/example/datphongkhachsan/LoginActivity.java
@@ -2,13 +2,25 @@ package com.example.datphongkhachsan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView tvToSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvToSignUp = findViewById(R.id.tvToSignUp);
+        tvToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent integer = new Intent(LoginActivity.this, SignUPActivity.class);
+                startActivity(integer);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/datphongkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datphongkhachsan/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Write a message to the database
+    // https://hothel-database-default-rtdb.asia-southeast1.firebasedatabase.app
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://hothel-database-default-rtdb.asia-southeast1.firebasedatabase.app");
+    DatabaseReference myRef = database.getReference("message");
+//    myRef.setValue("Hello, World!");
+
+
 }
