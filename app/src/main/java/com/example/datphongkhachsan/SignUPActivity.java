23d728ad615fe3417cb142ed264cde31ad818c3e
Chu Quang Hải <76799685+quanghaik2@users.noreply.github.com>
2023-02-14 08:20:06 +0700

    login and sign up

---
+++ app/src/main/java/com/example/datphongkhachsan/SignUPActivity.java
@@ -0,0 +1,14 @@
+package com.example.datphongkhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_upactivity);
+    }
+}
