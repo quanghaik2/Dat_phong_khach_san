e44d94ba17f166ca9ed9c81e64c1f10af3353bd1
Chu Quang Hải <76799685+quanghaik2@users.noreply.github.com>
2023-02-14 08:29:47 +0700

    test

--- app/src/main/java/com/example/datphongkhachsan/SignUPActivity.java
+++ app/src/main/java/com/example/datphongkhachsan/SignUPActivity.java
@@ -3,9 +3,11 @@ package com.example.datphongkhachsan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUPActivity extends AppCompatActivity {
 
+    EditText  edtSignUpEmail,edtSignUpPassword,edtConfirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
