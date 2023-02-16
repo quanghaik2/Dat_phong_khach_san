20ee7a802f648bee549c77f39d11835e3d1a73f9
Chu Quang Hải <76799685+quanghaik2@users.noreply.github.com>
2023-02-16 13:35:57 +0700

    hide password

--- app/src/main/java/com/example/datphongkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datphongkhachsan/MainActivity.java
@@ -3,22 +3,26 @@ package com.example.datphongkhachsan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_upactivity);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
     }
 
     // Write a message to the database
     // https://hothel-database-default-rtdb.asia-southeast1.firebasedatabase.app
-    FirebaseDatabase database = FirebaseDatabase.getInstance("https://hothel-database-default-rtdb.asia-southeast1.firebasedatabase.app");
-    DatabaseReference myRef = database.getReference("message");
+
 //    myRef.setValue("Hello, World!");
 
 
--- app/src/main/java/com/example/datphongkhachsan/SignUPActivity.java
+++ app/src/main/java/com/example/datphongkhachsan/SignUPActivity.java
@@ -3,17 +3,28 @@ package com.example.datphongkhachsan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignUPActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://hotel-database-e38fc-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
     EditText  edtSignUpEmail,edtSignUpPassword,edtConfirmPassword;
     Button btnSignUp;
     TextView tvToLogin;
+    ImageView hidePass,hidePassConfirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +34,9 @@ public class SignUPActivity extends AppCompatActivity {
         edtConfirmPassword = findViewById(R.id.edtConfirmPassword);
         btnSignUp = findViewById(R.id.btnSignUp);
         tvToLogin = findViewById(R.id.tvToLogin);
+        hidePass = findViewById(R.id.hidePass);
+        hidePassConfirm = findViewById(R.id.hidePassConfirm);
+
 
         tvToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +45,69 @@ public class SignUPActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        hidePass.setOnClickListener(new View.OnClickListener() {
+//            Drawable drawable = hidePass.getDrawable();
+
+            @Override
+            public void onClick(View v) {
+                if(edtSignUpPassword.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())){
+//                    hidePass.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_visibility_off_24));
+                    edtSignUpPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    hidePass.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                }
+                else {
+//                    hidePass.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_visibility_24));
+                    edtSignUpPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    hidePass.setImageResource(R.drawable.ic_baseline_visibility_24);
+                }
+            }
+        });
+
+        hidePassConfirm.setOnClickListener(new View.OnClickListener() {
+//            Drawable drawable = hidePass.getDrawable();
+
+            @Override
+            public void onClick(View v) {
+                if(edtConfirmPassword.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())){
+//                    hidePass.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_visibility_off_24));
+                    edtConfirmPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    hidePassConfirm.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                }
+                else {
+//                    hidePass.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_visibility_24));
+                    edtConfirmPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    hidePassConfirm.setImageResource(R.drawable.ic_baseline_visibility_24);
+                }
+            }
+        });
+
+
+
+
+
+//        btnSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                // lay du lieu tu Edit Text
+//                String email = edtSignUpEmail.getText().toString();
+//                String password = edtSignUpPassword.getText().toString();
+//                String confirm = edtConfirmPassword.getText().toString();
+//
+//                if(password.equals(confirm)){
+//                    databaseReference.child("email").setValue(email);
+//                    databaseReference.child("password").setValue(password);
+//                }
+//
+//                finish();
+//            }
+//        });
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("messages");
+//
+//        myRef.setValue("Hello World!");
+
     }
 }
