590e67b72f32bd66fece31565d3cfedb77d67e7f
vidic-nguyen <dichthucgg@gmail.com>
2023-02-23 12:03:34 +0700

    listviewhome

---
+++ app/src/main/java/com/example/datphongkhachsan/Chi_tiet_phong.java
@@ -0,0 +1,14 @@
+package com.example.datphongkhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chi_tiet_phong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_phong);
+    }
+}
