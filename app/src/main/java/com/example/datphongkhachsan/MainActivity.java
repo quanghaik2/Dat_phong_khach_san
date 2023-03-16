c4b33eb780df4cf32392e4767640611c01ea8daf
Chu Quang Hải <76799685+quanghaik2@users.noreply.github.com>
2023-03-16 09:32:43 +0700

    get layout

--- app/src/main/java/com/example/datphongkhachsan/MainActivity.java
+++ app/src/main/java/com/example/datphongkhachsan/MainActivity.java
@@ -13,19 +13,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvLogin;
     Button btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
